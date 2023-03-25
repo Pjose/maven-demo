69e29f3064e7312e3742e9e3fc2bd4b20f8eb7ec
PJ Mwangi <pjmwangi@hotmail.com>
2023-03-25 09:55:10 -0500

    Update Calculator.java
    Edit main comments for the class and square method.

--- maven-demo/src/main/java/com/pj/learn/Calculator.java
+++ maven-demo/src/main/java/com/pj/learn/Calculator.java
@@ -1,14 +1,14 @@
 package com.pj.learn;
 
 /**
- * This {@code Calculator} class implements some simple calculator type methods.
+ * This {@code Calculator} class implements some simple calculator methods.
  * @since  3/24/2023
  * @author PJ Mwangi
  */
 public class Calculator {
 	
 	/**
-	 * Calculates the square of a number(x) passed as parameter.
+	 * Calculates the square of a number(x) passed as a parameter.
 	 * <pre> Formula: square(x) = x * x </pre>
 	 * @param x number to square
 	 * @return  the square of the parameter x
